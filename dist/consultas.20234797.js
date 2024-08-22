f262c0277a04991d7a94b21bb3f5f15947273cd6
nazyri <168209590+nazyri@users.noreply.github.com>
2024-08-22 11:28:36 -0600

    COMMIT2

--- dist/consultas.20234797.js
+++ dist/consultas.20234797.js
@@ -1,2 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+selectBtn = wrapper.querySelector(".select-btn");
+searchInp = wrapper.querySelector("input");
+options = wrapper.querySelector(".options");
+let countries = [
+    "holaaaa",
+    "adios",
+    "chao",
+    "ayer",
+    "hoy"
+];
+function addCountry(selectedCountry) {
+    options.innerHTML = "";
+    countries.forEach((country)=>{
+        let inSelected = country == selectedCountry ? "selected" : "";
+        let li = '<li onclick = "updateName(this)" class="${isSelected}">${country}</li>';
+        options.insertAdjacentHTML("beforeend", li);
+    });
+}
+addCountry();
+function updateName(selectedLi) {
+    searchInp.value = "";
+    wrapper.classList.remove("active");
+    selectBtn.firstElementChild.innerText = selectedLi.innerText;
+}
 
 //# sourceMappingURL=consultas.20234797.js.map
