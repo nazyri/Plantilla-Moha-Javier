e75c62a0b78409d8e95dd0ec138990d1eaaa7e18
Javier348solis <javiersolisretana@gmail.com>
2024-08-23 09:27:49 -0600

    Co-authored-by: nazyri <nazyri@users.noreply.github.com>

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,3 +1,31 @@
+//Post
+async function darDatos(){ 
+    try {
+        let tarea ={
+            id:Date.now(),
+            nombre:inputTask.value,
+            estado:false
+        }
+        const respuesta = await fetch("http://localhost:3002/users",{
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify(tarea)
+        })
+        let data = await respuesta.json()
+        getDatos()
+         console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+
+
 export async function getDatos() {
     try {
         const response = await fetch('http://localhost:3001/users');
